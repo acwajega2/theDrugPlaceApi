ea9651d5664dadfedab2b64da85542e24ea13fbe
Chris Wajega <achriswajega@gmail.com>
2023-11-14 18:19:05 +0300

    Adds cors

--- src/main/java/com/thedrugplace/com/DrugPlaceSalesApi/beans/WebSecurityConfiguration.java
+++ src/main/java/com/thedrugplace/com/DrugPlaceSalesApi/beans/WebSecurityConfiguration.java
@@ -1,55 +1,33 @@
-//package com.thedrugplace.com.DrugPlaceSalesApi.beans;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.util.logging.Logger;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    private static final Logger logger = Logger.getLogger(WebSecurityConfiguration.class.getName());
-//
-//    /**
-//     * Configure security settings for the HTTP requests.
-//     *
-//     * @param http HttpSecurity configuration.
-//     * @throws Exception Exception thrown if configuration fails.
-//     */
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.csrf()
-//                .disable()
-//                .headers()
-//                .frameOptions()
-//                .disable();
-//
-//        // Log configuration
-//        logger.info("HTTP security configuration successfully applied.");
-//    }
-//
-//    /**
-//     * Configure Cross-Origin Resource Sharing (CORS) settings for the application.
-//     *
-//     * @return WebMvcConfigurer to handle CORS configuration.
-//     */
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                // Configure allowed origins for CORS
-//                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-//                logger.info("CORS configuration applied for 'http://localhost:3000'");
-//            }
-//        };
-//    }
-//}
+package com.thedrugplace.com.DrugPlaceSalesApi.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.logging.Logger;
+
+@Configuration
+public class WebSecurityConfiguration  {
+    private static final Logger logger = Logger.getLogger(WebSecurityConfiguration.class.getName());
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION);
+            }
+        };
+    }
+
+
+}
