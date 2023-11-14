90699c78c4ce5a6f6fc92971991e3549fb41c73a
Chris Wajega <achriswajega@gmail.com>
2023-11-14 18:31:08 +0300

    Added rights

--- src/main/java/com/thedrugplace/com/DrugPlaceSalesApi/beans/WebSecurityConfiguration.java
+++ src/main/java/com/thedrugplace/com/DrugPlaceSalesApi/beans/WebSecurityConfiguration.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.logging.Logger;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfiguration  {
@@ -29,5 +34,16 @@ public class WebSecurityConfiguration  {
         };
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated());
+
+        // http....;
+
+        return http.build();
+    }
 
 }
